09dd39d0e2aa05e04cde31ca144ae94a62eaed97
John Garrison <splendidonkey@gmail.com>
2023-07-12 08:39:49 -0500

    added persistence of data via local storage. Added input value checks.

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,11 @@
 const titleTodaysDate = dayjs().format(`dddd[, ]MMMM[ ]D`);
 $(`#todays-date`).text(titleTodaysDate);
 
-// This changes row background color to gray on click event.
-$(`tr`).on(`click`, function () {
-  if ($(this).hasClass(`table-active`)) {
-    $(this).removeClass(`table-active`);
-  } else {
-    $(this).addClass(`table-active`);
-  }
-});
-
 // This function applies background styling to table row elements depending on current time of day
 // using bootstrap table, so applying bootstrap table contextual classes:
 // *  https://getbootstrap.com/docs/4.0/content/tables/#contextual-classes
 
 function setTable() {
-  console.log(dayjs().get(`hour`));
-
   const rowHourCheckStr = `tr-hour-`;
   const currentHour = dayjs().get(`hour`);
   
@@ -28,13 +17,53 @@ function setTable() {
   
     if (i < currentHour) {
       $(rowToCheck).addClass(`table-active`);
-      console.log(`added`);
+    } else if (i === currentHour) {
+      $(rowToCheck).addClass(`table-danger`);
+    } else {
+      $(rowToCheck).addClass(`table-success`);
     }
   
     currentTableHour++;
-    console.log(rowToCheck)
-    // if (dayjs().get(`hour`))
+  }
+
+  // loop through input field row slots and set text if any stored
+  for (let i = 0; i <= 11; i++) {
+    let taskToGet = `slot${i}`;
+    if(getTask(taskToGet) !== null) {
+      let slotValue = getTask(taskToGet);
+      // console.log(`slot: ${taskToGet} task: ${getTask(taskToGet)}`);
+      $(`#${taskToGet}`).val(slotValue);
+    }
   }
 }
+
+// add row input field text to corresponding local storage slot
+function saveTask(slotID, inputText) {
+  console.log(`saving ${inputText} in ${slotID}`);
+
+  localStorage.setItem(slotID, inputText);
+}
+
+// get value from local storage slot
+function getTask(slotID) {
+    return localStorage.getItem(slotID);
+}
+
+// save row input to local storage on click event
+$(`.btn-primary`).on(`click`, function() {
+  let rowInputField = $(this).parent().siblings(`.task`).children();
+  // rowInputField.css({"color": "red", "border": "2px solid red"});
+
+  // console.log(rowInputField.val()); // this is the value of the input for the save
+  // console.log(rowInputField.attr("id")); // this is the ID of the input for the save to be used as storage key name
+
+  // if input field is empty, alert user and do not perform save
+  if (rowInputField.val() === ``) {
+    alert(`Please input data into ${rowInputField.attr(`id`)} to perform save.`)
+  } else {
+    // add text in input slot to local storage
+    saveTask(rowInputField.attr("id"), rowInputField.val());
+  }
+});
 
 setTable();
